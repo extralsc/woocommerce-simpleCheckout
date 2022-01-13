64875f38f301423033e70266a91a0744126124c3
Christopher Salame <christophersalame@gmail.com>
2022-01-13 20:32:19 +0100

    Added version 2 of checkout

--- templates/fc/checkout/review-order-section.php
+++ templates/fc/checkout/review-order-section.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Order review section
  *
@@ -17,48 +18,63 @@
  * @wc-original checkout/form-checkout.php
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-$attributes_str = implode( ' ', array_map( array( FluidCheckout::instance(), 'map_html_attributes' ), array_keys( $attributes ), $attributes ) );
-$attributes_inner_str = implode( ' ', array_map( array( FluidCheckout::instance(), 'map_html_attributes' ), array_keys( $attributes_inner ), $attributes_inner ) );
+$attributes_str = implode(' ', array_map(array(FluidCheckout::instance(), 'map_html_attributes'), array_keys($attributes), $attributes));
+$attributes_inner_str = implode(' ', array_map(array(FluidCheckout::instance(), 'map_html_attributes'), array_keys($attributes_inner), $attributes_inner));
 ?>
 
-<?php do_action( 'fc_checkout_before_order_review', $is_sidebar_widget ); ?>
+<?php do_action('fc_checkout_before_order_review', $is_sidebar_widget); ?>
 
-<div <?php echo $attributes_str; // WPCS: XSS ok. ?>>
+<div <?php echo $attributes_str; // WPCS: XSS ok. 
+		?>>
 
-	<div <?php echo $attributes_inner_str; // WPCS: XSS ok. ?>>
+	<div <?php echo $attributes_inner_str; // WPCS: XSS ok. 
+			?>>
 
-		<?php do_action( 'fc_checkout_before_order_review_inside', $is_sidebar_widget ); ?>
+		<?php do_action('fc_checkout_before_order_review_inside', $is_sidebar_widget); ?>
 
-		<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
+		<?php do_action('woocommerce_checkout_before_order_review_heading'); ?>
 
-		<h3 class="fc-checkout-order-review-title fc-step__substep-title"><?php echo esc_html( $order_review_title ); ?></h3>
+		<h3 class="fc-checkout-order-review-title fc-step__substep-title"><?php echo esc_html($order_review_title); ?></h3>
 
-		<?php if ( $is_sidebar_widget && apply_filters( 'fc_order_summary_display_desktop_edit_cart_link', true ) ) : ?>
-			<a href="<?php echo esc_url( wc_get_cart_url() ); ?>" class="fc-checkout-order-review__edit-cart"><?php echo esc_html( __( 'Edit Cart', 'fluid-checkout' ) ); ?></a>
+		<?php if ($is_sidebar_widget && apply_filters('fc_order_summary_display_desktop_edit_cart_link', true)) : ?>
+			<a href="<?php echo esc_url(wc_get_cart_url()); ?>" class="fc-checkout-order-review__edit-cart"><?php echo esc_html(__('Edit Cart', 'fluid-checkout')); ?></a>
 		<?php endif; ?>
 
-		<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+		<?php do_action('woocommerce_checkout_before_order_review'); ?>
 
 		<div id="order_review" class="woocommerce-checkout-review-order">
-			<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+			<?php do_action('woocommerce_checkout_order_review'); ?>
 		</div>
 
-		<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+		<?php do_action('woocommerce_checkout_after_order_review'); ?>
 
-		<?php if ( $is_sidebar_widget ) : ?>
-			<?php do_action( 'fc_checkout_order_review_sidebar_before_actions', $is_sidebar_widget ); ?>
+		<?php if ($is_sidebar_widget) : ?>
+			<?php do_action('fc_checkout_order_review_sidebar_before_actions', $is_sidebar_widget); ?>
 
 			<div class="fc-checkout-order-review__actions-mobile">
-				<a href="<?php echo esc_url( wc_get_cart_url() ); ?>" class="fc-checkout-order-review__edit-cart"><?php echo esc_html( __( 'Edit Cart', 'fluid-checkout' ) ); ?></a>
-				<button type="button" class="fc-checkout-order-review__close-order-summary <?php echo esc_attr( apply_filters( 'fc_order_summary_continue_button_classes', 'button' ) ); ?>" data-flyout-close aria-label="<?php echo esc_html( __( 'Close and continue with checkout', 'fluid-checkout' ) ); ?>"><?php echo esc_html( __( 'Continue', 'fluid-checkout' ) ); ?></button>
+				<a href="<?php echo esc_url(wc_get_cart_url()); ?>" class="fc-checkout-order-review__edit-cart"><?php echo esc_html(__('Edit Cart', 'fluid-checkout')); ?></a>
+				<button type="button" class="fc-checkout-order-review__close-order-summary <?php echo esc_attr(apply_filters('fc_order_summary_continue_button_classes', 'button')); ?>" data-flyout-close aria-label="<?php echo esc_html(__('Close and continue with checkout', 'fluid-checkout')); ?>"><?php echo esc_html(__('Continue', 'fluid-checkout')); ?></button>
 			</div>
 		<?php endif; ?>
 
-		<?php do_action( 'fc_checkout_after_order_review_inside', $is_sidebar_widget ); ?>
+		<?php if (get_option('fc_enable_checkout_place_order_sidebar', 'no') !== 'no') : ?>
+
+			<h3 class="fc-checkout-order-payment-method fc-step__substep-title"><?php echo esc_html(__('Payment method', 'fluid-checkout')); ?></h3>
+			<?php
+			wc_get_template(
+				'fc/checkout/form-payment.php',
+				array(
+					'checkout'          => WC()->checkout(),
+				)
+			);
+			?>
+		<?php endif; ?>
+
+		<?php do_action('fc_checkout_after_order_review_inside', $is_sidebar_widget); ?>
 
 	</div>
 </div>
 
-<?php do_action( 'fc_checkout_after_order_review', $is_sidebar_widget ); ?>
+<?php do_action('fc_checkout_after_order_review', $is_sidebar_widget); ?>
